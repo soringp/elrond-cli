bdde3f9147f00d8e2be605d684ff6209f2322aa4
Sebastian Johnsson <sebastian.johnsson@gmail.com>
2019-12-19 19:29:00 +0700

    Use curl instead of wget for installation

--- scripts/erd.sh
+++ scripts/erd.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Installing Elrond CLI (erd)"
-rm -rf erd && wget --quiet http://tools.elrond.com.s3.amazonaws.com/release/linux-x86_64/erd && chmod u+x erd
-mkdir -p config && cd config && rm -rf economics.toml && wget --quiet https://raw.githubusercontent.com/ElrondNetwork/elrond-config/master/economics.toml
+curl -LOs http://tools.elrond.com.s3.amazonaws.com/release/linux-x86_64/erd > /dev/null && chmod u+x erd
+mkdir -p config && cd config && rm -rf economics.toml && curl -LOs https://raw.githubusercontent.com/ElrondNetwork/elrond-config/master/economics.toml > /dev/null
 echo "Elrond CLI is now ready to use!"
 echo "Invoke it using ./erd - see ./erd --help for all available commands and options"
